a382b1b2b06d42d52ca122b86170126bac459bfe
Rafi12234 <shajidulkabir12345@gmail.com>
2026-04-24 00:41:56 +0600

    frontend: Add OffensesPage component

---
+++ frontend/src/pages/OffensesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './OffensesPage.css';
+
+const OffensesPage = () => {
+  return (
+    <div className="offenses-page">
+      <h2>Offense Details</h2>
+      <p>Detailed view of all offenses detected during exam monitoring.</p>
+    </div>
+  );
+};
+
+export default OffensesPage;
